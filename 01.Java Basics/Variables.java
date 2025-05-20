fca5027d3cd45ee0654a75f9197498b42c06e9f4
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-21 00:48:47 +0600

    Variables

---
+++ 01.Java Basics/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+        int num1 = 10, num2 = 15, sum, sub, mul, div, mod, exp;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+
+        System.out.println("The sub of " + num1 + " and " + num2 + " is " + (num1 - num2));
+
+        System.out.println("The mul of " + num1 + " and " + num2 + " is " + (num1 * num2));
+
+        System.out.println("The div of " + num1 + " and " + num2 + " is " + (num1 / num2));
+
+        System.out.println("The mod of " + num1 + " and " + num2 + " is " + (num1 % num2));
+        
+        System.out.println("The exp of " + num1 + " and " + num2 + " is " + (num1 * num2) / (num1 + num2));
+
+    }
+}
