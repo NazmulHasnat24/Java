f8189ba4cc8d048f1fd5b82d8260ebd6b5c18402
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-21 00:34:51 +0600

    Printing z

---
+++ 01.Java Basics/Printing.java
@@ -0,0 +1,14 @@
+//print z with star
+
+public class Printing {
+    public static void main(String[] args) {
+        System.out.println("Printing a Z : ");
+        
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*    ");
+        System.out.println("*****");
+    }
+}
