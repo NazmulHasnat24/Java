2e9a55d5892c2a21dbd57dae93e864101a04e26c
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-21 00:57:23 +0600

    Conditions

---
+++ 01.Java Basics/Conditions.java
@@ -0,0 +1,28 @@
+public class Conditions {
+ 
+    public static void main(String[] args) {
+        
+        int num1 = 20;
+        int num2 = 30;
+
+        if(num1 == num2){
+
+            System.out.println(num1 + " is equal to " + num2);
+
+        }
+        else if(num1 > num2){
+
+            System.out.println(num1 + " is greater than " + num2);
+
+        }
+        else{
+
+            System.out.println(num1 + " is smaller than " + num2);
+
+        }
+
+        System.out.println("You Have learnt conditions here. Well Done !!!");
+
+    }
+
+}
