02d64272dc82c189d339bb826b3c93ed4876ad2d
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-21 22:21:04 +0600

    Loops

--- 01.Java Basics/Loops.java
+++ 01.Java Basics/Loops.java
@@ -6,17 +6,29 @@ public class Loops {
         
         Scanner scan = new Scanner(System.in);
 
+        int counter = 0;
+
         System.out.println("Enter a number here : ");
 
         int num1 = Integer.parseInt(scan.nextLine());
          
+        System.out.println("Printing using for loop :");
+
         for(int i = 0; i < num1; i++){
 
-            System.out.println(i);
+            System.out.print(i + " ");
 
         }
 
-        String name = 
+        System.out.println();
+
+        System.out.println("Printing using while loop :");
+        while(counter < num1){
+
+            System.out.print(counter + " ");
+
+            counter++;
+        }
 
     }
 }
