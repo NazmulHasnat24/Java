1db89be054b2ab3c8b72a37d8dd204763a92fc81
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-22 00:22:44 +0600

    Star Pattern 2

---
+++ 01.Java Basics/Pattern2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern2 {
+        public static void main(String[] args) {
+
+        System.out.println("Enter a number here : ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+         
+        for(int row = 0; row < number; row++  ){
+            for(int col = number; col > row; col--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
